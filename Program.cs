0f8f188a81a8db63de4c2f479d43bc8ac2b2a036
Spitfiremk5 <31819694+Spitfiremk5@users.noreply.github.com>
2020-03-18 17:46:57 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+
+namespace CsharpConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type some stuff and it will appear in a text file");
+            while (true)
+            {
+                string path = @"plik.txt";
+
+                StreamWriter sw;
+
+                if (!File.Exists(path))
+                {
+                    sw = File.CreateText(path);
+                   
+                }
+                else
+                {
+                    sw = new StreamWriter(path, true);
+                   
+
+                }
+
+                string s = Console.ReadLine();
+                sw.WriteLine(s);
+                sw.Close();
+                StreamReader sr = File.OpenText(path);
+                string yeetus = "";
+
+                int loop = 1;
+
+                while ((yeetus = sr.ReadLine()) != null)
+                {
+
+                }
+                sr.Close();
+            }
+               
+            
+        }
+    }
+}
